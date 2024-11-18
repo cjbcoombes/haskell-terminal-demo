6b54a0ae7061e63a2efeaf24e317b363aab52df1
Christopher-Coombes <cjbcoombes@gmail.com>
2024-11-17 23:07:15 -0500

    finish gameplay and instructions

--- src/Game.hs
+++ src/Game.hs
@@ -10,6 +10,7 @@ import Control.Applicative (liftA2)
 import Control.Monad (void, unless)
 import System.IO (stdout, hFlush)
 import Data.List (intercalate)
+import System.Random (randomIO)
 
 box :: Int -> Int -> String
 box w h = "┌" ++ bar ++ "┐\n" ++ rows ++ "└" ++ bar ++ "┘\n"
@@ -27,9 +28,9 @@ splitbox w h = "┌" ++ divide "┬" '─' ++ "┐\n" ++ rows ++ "└" ++ divide
 width :: Int
 width = 70
 sidewidth :: Int
-sidewidth = 20
+sidewidth = 21
 height :: Int
-height = 20
+height = 21
 
 outputPos :: (Int, Int)
 outputPos = (1, 1)
@@ -57,7 +58,7 @@ parseInput s = mapReject (const "error") $ fst <$> runParser (exprParser <* eofP
 
 scoreExpr :: Expr -> Int
 scoreExpr (EInt i) = fromIntegral i
-scoreExpr (EVar v) = 5
+scoreExpr (EVar v) = 3
 scoreExpr (ENeg e) = scoreExpr e
 scoreExpr (EAdd a b) = scoreExpr a + scoreExpr b + 1
 scoreExpr (EMul a b) = scoreExpr a + scoreExpr b + 4
@@ -67,7 +68,7 @@ scoreExpr (EExp a b) = scoreExpr a + scoreExpr b + 7
 scoreExpr (EWhere a m) = scoreExpr a + foldr ((+) . scoreExpr) 0 m
 
 colorExpr :: Expr -> IO ()
-colorExpr x = write x >> setSGR [Reset] >> putStrLn ""
+colorExpr x = write x >> setSGR [Reset]
     where
         write (EInt i) = printColor icol $ show i
         write (EVar v) = printColor vcol v
@@ -94,45 +95,104 @@ colorExpr x = write x >> setSGR [Reset] >> putStrLn ""
         wcol = Blue
         comcol = White
 
-colorEval :: Expr -> IO ()
-colorEval e = maybe wrong draw (eval e) >> setSGR [Reset]
+colorEval :: Int -> Expr -> IO ()
+colorEval tgt e = maybe wrong (draw . fromIntegral) (eval e) >> setSGR [Reset]
     where wrong = printColor Red "(!)"
           draw x | x > 1000 = printColor Red ">1000"
                  | x < 0 = printColor Red "< 0"
-                 | otherwise = printColor Green (show x)
+                 | x == tgt = printColor Green (show x)
+                 | otherwise = printColor Red (show x)
 
-inputLoop :: Int -> IO ()
-inputLoop x | x >= height - 1 = return ()
-            | otherwise = do
-                setCursorPos cursorPos
-                putStr $ ">" ++ replicate (width - 1) ' '
-                setCursorPos typingPos
-                hFlush stdout
-                line <- take width <$> getLine
-                unless (line == "quit") (do
-                    let expr = parseInput line
-                    setCursorPos $ (+ x) <$> outputPos
-                    case expr of
-                        Accept e -> colorExpr e
-                        Reject e -> printColor Red "(!)"
-                    setSGR [Reset]
-                    setCursorPos $ (+ x) <$> evalPos
-                    case expr of
-                        Accept e -> colorEval e
-                        Reject e -> printColor Red "(!)"
-                    setSGR [Reset]
-                    setCursorPos $ (+ x) <$> scorePos
-                    case expr of
-                        Accept e -> printColor Magenta (show $ scoreExpr e)
-                        Reject e -> printColor Red "(!)"
-                    setSGR [Reset]
+inputLoop :: Int -> Int -> IO ()
+inputLoop tgt x | x >= height - 1 = return ()
+                | otherwise = do
+                    setCursorPos cursorPos
+                    putStr $ ">" ++ replicate (width - 1) ' '
+                    setCursorPos typingPos
                     hFlush stdout
-                    inputLoop (x + 1))
+                    line <- take width <$> getLine
+                    unless (line == "quit") (do
+                        let expr = parseInput line
+                        setCursorPos $ (+ x) <$> outputPos
+                        case expr of
+                            Accept e -> colorExpr e
+                            Reject e -> printColor Red "(!)"
+                        setSGR [Reset]
+                        setCursorPos $ (+ x) <$> evalPos
+                        case expr of
+                            Accept e -> colorEval tgt e
+                            Reject e -> printColor Red "(!)"
+                        setSGR [Reset]
+                        setCursorPos $ (+ x) <$> scorePos
+                        case expr of
+                            Accept e -> printColor Magenta (show $ scoreExpr e)
+                            Reject e -> printColor Red "(!)"
+                        setSGR [Reset]
+                        hFlush stdout
+                        inputLoop tgt (x + 1))
 
 playGame :: IO ()
 playGame = do
     clearScreen
     setCursorPos (0, 0)
     putStr (splitbox [width, 6, 6, sidewidth] height)
-    inputLoop 0
+
+    rand <- randomIO :: IO Int
+    let target = 20 + rand `mod` 980
+
+    setCursorPos $ (+1) <$> instrPos
+    putStr "    Target: " >> printColor Green (show target) >> setSGR [Reset]
+    setCursorPos $ (+3) <$> instrPos
+    putStr " Write an expression"
+    setCursorPos $ (+4) <$> instrPos
+    putStr " that evaluates to"
+    setCursorPos $ (+5) <$> instrPos
+    putStr " the given target."
+    setCursorPos $ (+6) <$> instrPos
+    putStr " Minimize cost."
+    setCursorPos $ (+8) <$> instrPos
+    printColor Blue "    where" >> printColor White " = " >> printColor Green "free"
+    setCursorPos $ (+9) <$> instrPos
+    printColor Yellow "       ()" >> printColor White " = " >> printColor Green "free"
+    setCursorPos $ (+10) <$> instrPos
+    printColor Cyan "        #" >> printColor White " = " >> printColor Yellow "value"
+    setCursorPos $ (+11) <$> instrPos
+    printColor Red "        v" >> printColor White " = " >> printColor Red "3"
+    setCursorPos $ (+12) <$> instrPos
+    printColor White "        + = " >> printColor Red "1"
+    setCursorPos $ (+13) <$> instrPos
+    printColor White "        - = " >> printColor Red "1"
+    setCursorPos $ (+14) <$> instrPos
+    printColor White "        / = " >> printColor Red "2"
+    setCursorPos $ (+15) <$> instrPos
+    printColor White "        * = " >> printColor Red "4"
+    setCursorPos $ (+16) <$> instrPos
+    printColor White "        ^ = " >> printColor Red "7"
+    setCursorPos $ (+18) <$> instrPos
+    printColor White " Ex:"
+    setCursorPos $ (+19) <$> instrPos
+    printColor White " -" >> printColor Yellow "(" >> printColor Red "x" >> printColor White " - " 
+        >> printColor Cyan "7" >> printColor Yellow ")" >> printColor Blue " where " >> printColor Red "x" 
+        >> printColor White "=" >> printColor Cyan "2"
+    setSGR [Reset]
+
+    inputLoop target 0
     setCursorPos quitPos
+
+--                    |
+--    Target=[...]
+--
+--Write an expression
+--that evaluates to
+--the given target.
+--Minimize cost.
+--
+--    where = free
+--       () = free
+--        # = value
+--        v = 3
+--        + = 1      
+--        - = 1
+--        / = 2
+--        * = 4
+--        ^ = 7
